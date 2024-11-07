8e14b4ca0988b55273d2d46da8f65128f163928b
Samuel Ajayi <samuelajayi554@gmail.com>
2024-11-07 01:37:58 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 import Header from "./components/Header";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Button from "./components/Button";
 
 const books = [
